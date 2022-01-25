4957b7403e85b1366ac5e8af0b290541799e9193
Jakob Jordan <jakobjordan@posteo.de>
2022-01-25 12:48:56 +0100

    Optimize normal operations, separate macros for normal and inplace operations

--- arraymath/src/lib.rs
+++ arraymath/src/lib.rs
@@ -1,25 +1,32 @@
 macro_rules! binary_op_prototype {
-    ($function_name: ident, $function_name_inplace: ident,
-     $input_inner: ty, $output: ty) => {
+    ($function_name: ident, $input_inner: ty, $output: ty) => {
         fn $function_name(&self, other: &[$input_inner]) -> $output;
-        fn $function_name_inplace(&mut self, other: &[$input_inner]);
+    };
+}
+
+macro_rules! binary_op_inplace_prototype {
+    ($function_name: ident, $input_inner: ty, $output: ty) => {
+        fn $function_name(&mut self, other: &[$input_inner]);
     };
 }
 
 macro_rules! binary_op {
-    ($function_name: ident, $function_name_inplace: ident,
-     $op: expr, $input_inner: ty, $output: ty) => {
+    ($function_name: ident, $op: expr, $input_inner: ty, $output: ty) => {
         #[inline(always)]
         fn $function_name(&self, other: &[$input_inner]) -> $output {
-            let mut res = self.clone();
+            let mut res: [T; N] = [self[0]; N];
             for i in 0..self.len() {
-                $op(&mut res[i], other[i]);
+                res[i] = $op(self[i], other[i]);
             }
             res
         }
+    };
+}
 
+macro_rules! binary_op_inplace {
+    ($function_name: ident, $op: expr, $input_inner: ty, $output: ty) => {
         #[inline(always)]
-        fn $function_name_inplace(&mut self, other: &[$input_inner]) {
+        fn $function_name(&mut self, other: &[$input_inner]) {
             for i in 0..self.len() {
                 $op(&mut self[i], other[i]);
             }
@@ -31,19 +38,30 @@ pub trait ArrayMath {
     type InputInner;
     type Output;
 
-    binary_op_prototype!(add, add_assign, Self::InputInner, Self::Output);
-    binary_op_prototype!(sub, sub_assign, Self::InputInner, Self::Output);
+    binary_op_prototype!(add, Self::InputInner, Self::Output);
+    binary_op_inplace_prototype!(add_assign, Self::InputInner, Self::Output);
+    binary_op_prototype!(sub, Self::InputInner, Self::Output);
+    binary_op_inplace_prototype!(sub_assign, Self::InputInner, Self::Output);
 }
 
 impl<T, const N: usize> ArrayMath for [T; N]
 where
-    T: Copy + std::ops::AddAssign + std::ops::SubAssign,
+    T: Copy
+        + std::ops::Add<Output = T>
+        + std::ops::AddAssign
+        + std::ops::Sub<Output = T>
+        + std::ops::SubAssign,
 {
     type InputInner = T;
     type Output = [T; N];
 
     binary_op!(
         add,
+        |lhs: T, rhs: T| lhs + rhs,
+        Self::InputInner,
+        Self::Output
+    );
+    binary_op_inplace!(
         add_assign,
         |lhs: &mut T, rhs: T| lhs.add_assign(rhs),
         Self::InputInner,
@@ -51,6 +69,11 @@ where
     );
     binary_op!(
         sub,
+        |lhs: T, rhs: T| lhs - rhs,
+        Self::InputInner,
+        Self::Output
+    );
+    binary_op_inplace!(
         sub_assign,
         |lhs: &mut T, rhs: T| lhs.sub_assign(rhs),
         Self::InputInner,
