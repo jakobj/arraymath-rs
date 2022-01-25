f2f373acd7e7f7ab5de761427498ae1d4e1bb7c2
Jakob Jordan <jakobjordan@posteo.de>
2022-01-25 13:50:54 +0100

    Let main reflect the readme example

--- arraymath/src/bin/main.rs
+++ arraymath/src/bin/main.rs
@@ -1,10 +1,11 @@
 use arraymath::ArrayMath;
 
 fn main() {
-    let mut a = [1.2, 1.3, 1.4];
+    let a = [1.2, 1.3, 1.4];
     let b = [1.3, 1.5, 2.4];
     println!("{:?}", a.add(&b));
     println!("{:?}", a.sub(&b));
+    let mut a = [1.2, 1.3, 1.4];
     a.add_assign(&b);
     println!("{:?}", a);
 }
